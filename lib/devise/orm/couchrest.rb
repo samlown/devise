987fc67e0585c21ea0ffe159fe4489b1ec357675
Sam Lown <me@samlown.com>
2010-11-19 00:40:49 +0100

    Renaming to match couchrest model

---
+++ lib/devise/orm/couchrest.rb
@@ -0,0 +1,45 @@
+module Devise
+  module Orm
+    module CouchRest
+      module Hook
+        def devise_modules_hook!
+          extend Schema
+          include Compatibility
+          yield
+          return unless Devise.apply_schema
+          devise_modules.each { |m| send(m) if respond_to?(m, true) }
+        end
+      end #Hook
+
+      module Schema
+        include Devise::Schema
+
+        # Tell how to apply schema methods.
+        def apply_devise_schema(name, type, options={})
+          #return unless Devise.apply_schema
+          property name, type, options
+        end
+      end #Module Schema 
+
+
+      module Compatibility
+
+        def save(options = nil)
+          if options.is_a?(Hash) && options[:validate] == false
+            save!
+          else
+            super()
+          end
+        end
+
+        def update_attributes(*args)
+          update(*args)
+        end
+
+      end
+
+    end #CouchRest
+  end #Orm
+end #Devise
+CouchRest::Model::Base.send(:extend, Devise::Models)
+CouchRest::Model::Base.send(:include, Devise::Models)
